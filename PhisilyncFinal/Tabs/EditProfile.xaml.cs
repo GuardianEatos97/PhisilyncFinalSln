b228cb6d3a5f1d0fe08efad1cd22d1fb1a1a66ad
TayiebK <150594024+mtk2608@users.noreply.github.com>
2024-05-16 15:10:12 +0200

    Profile Page
    - Edit Profile Created
    - Profile Details Page Created
    *Will be done by end of Business 17/04*

---
+++ PhisilyncFinal/Tabs/EditProfile.xaml.cs
@@ -0,0 +1,14 @@
+namespace PhisilyncFinal.Tabs;
+
+public partial class EditProfile : ContentPage
+{
+	public EditProfile()
+	{
+		InitializeComponent();
+	}
+
+    private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+}
