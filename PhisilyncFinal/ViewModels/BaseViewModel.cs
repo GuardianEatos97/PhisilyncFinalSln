b61339a5319266472a5329d8c10b6b44c7af7565
MTariqR <5100155@mypillar7uwc.ac.za>
2024-05-16 14:54:32 +0200

    Created BaseViewModel

---
+++ PhisilyncFinal/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhisilyncFinal.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual void Initialize()
+        {
+        }
+    }
+}
