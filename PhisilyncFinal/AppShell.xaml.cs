79913de417bfed63edc2580630f618a3ce824c51
thaverk <thaverk01@gmail.com>
2024-05-16 11:01:30 +0200

    logic for opening dashboard is done

--- PhisilyncFinal/AppShell.xaml.cs
+++ PhisilyncFinal/AppShell.xaml.cs
@@ -1,14 +1,38 @@
-﻿using PhisilyncFinal.Tabs;
+﻿using PhisilyncFinal.Models;
+using PhisilyncFinal.Tabs;
 
 namespace PhisilyncFinal
 {
+    
     public partial class AppShell : Shell
     {
+        public User user;
+        public User User
+        {
+            get { return user; }
+            set { user = value; }
+        }
         public AppShell()
         {
+            User = new User();
             InitializeComponent();
-            Routing.RegisterRoute("AthleteDash", typeof(AthleteDash));
-            Navigation.PushAsync(new AthleteDash());
+            LoadAthleteDash();
         }
+
+
+        public void LoadAthleteDash()
+        {
+            if (User.userID != null)
+            {
+                Routing.RegisterRoute("AthleteDash", typeof(AthleteDash));
+                Navigation.PushAsync(new AthleteDash());
+            }
+            else
+            { 
+            Routing.RegisterRoute("TellUsAboutYourself", typeof(TellUsPage));
+                Navigation.PushAsync(new TellUsPage());
+            }
+        }
+
     }
 }
