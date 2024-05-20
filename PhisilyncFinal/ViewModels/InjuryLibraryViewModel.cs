5e32f297ab0a123ed503d35ac93b6658965bba71
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-05-20 15:39:42 +0200

    Injury Library VM created

---
+++ PhisilyncFinal/ViewModels/InjuryLibraryViewModel.cs
@@ -0,0 +1,34 @@
+﻿//using CoreML;
+using PhisilyncFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhisilyncFinal.ViewModels
+{
+    public class InjuryLibraryViewModel
+    {
+        public IList<InjuryLibrary> Injury { get; private set; }
+
+        public InjuryLibraryViewModel() 
+        {
+            Injury = new List<InjuryLibrary>();
+
+            Injury.Add(new InjuryLibrary
+            {
+                InjuryName = "Baboon",
+                InjuryDescription = "Baboons are African and Arabian Old World monkeys belonging to the genus Papio, part of the subfamily Cercopithecinae.",
+                InjuryImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg",
+            });
+
+            Injury.Add(new InjuryLibrary
+            {
+                InjuryName = "Seated Monkey",
+                InjuryDescription = "This monkey is laid back and relaxed, and likes to watch the world go by. It doesn't make a lot of noise, often smiles mysteriously, and sleeps sitting up.",
+                InjuryImage = "moerat.jpg"
+            });
+        }
+    }
+}
--- PhisilyncFinal/Views/InjuryPage.xaml.cs
+++ PhisilyncFinal/Views/InjuryPage.xaml.cs
@@ -1,9 +1,14 @@
+using PhisilyncFinal.ViewModels;
 namespace PhisilyncFinal.Views;
 
 public partial class InjuryPage : ContentPage
 {
-	public InjuryPage()
+	InjuryLibraryViewModel _viewModel;
+	public InjuryPage(InjuryLibraryViewModel vm)
 	{
+		_viewModel = vm;
 		InitializeComponent();
+		//BindingContext _viewmodel;
+		BindingContext = new InjuryLibraryViewModel();
 	}
 }
