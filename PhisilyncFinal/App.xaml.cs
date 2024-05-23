61096ecfea16677bcbb9c75a0b75d3456b41ed7a
MTariqR <5100155@mypillar7uwc.ac.za>
2024-05-23 10:28:25 +0200

    Logic added to launch to the athlete dash if there is a user

--- PhisilyncFinal/App.xaml.cs
+++ PhisilyncFinal/App.xaml.cs
@@ -1,19 +1,33 @@
-﻿using PhisilyncFinal.Views;
+﻿using PhisilyncFinal.Models;
+using PhisilyncFinal.Services;
+using PhisilyncFinal.Views;
 
 namespace PhisilyncFinal
 {
     public partial class App : Application
     {
+        private LocalDb _database;
         public App()
         {
             InitializeComponent();
-
+            _database = new LocalDb();
             MainPage = new AppShell();
         }
 
         protected override async void OnStart()
         {
-            await Shell.Current.GoToAsync("///tellUsPage");
+            //await Shell.Current.GoToAsync("///tellUsPage");
+            //base.OnStart();
+            bool isUserLoggedIn = _database._dbConnection.Table<User>().Count() != 0;
+            
+            if (isUserLoggedIn)
+            {
+                await Shell.Current.GoToAsync("///athleteDash");
+            }
+            else
+            {
+                await Shell.Current.GoToAsync("///tellUsPage");
+            }
             base.OnStart();
         }
     }
