6dcf5714ff5a2530f0bf8d3f906b2d881769e453
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-05-27 10:42:36 +0200

    details page button fixed

--- PhisilyncFinal/Views/InjuryDetailsPage.xaml.cs
+++ PhisilyncFinal/Views/InjuryDetailsPage.xaml.cs
@@ -7,12 +7,8 @@ public partial class InjuryDetailsPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void GlossaryBtn_Clicked(object sender, EventArgs e)
+    private async void GlossaryBtn_Clicked(object sender, EventArgs e)
     {
-<<<<<<< HEAD
-
-=======
 		await Shell.Current.GoToAsync("GlossaryView");
->>>>>>> parent of d194181 (Glossary Navigation added)
     }
 }
