930ec3dd8f484fd4d55bf3d60a781bfdb69d48ae
TayiebK <150594024+mtk2608@users.noreply.github.com>
2024-05-30 14:03:05 +0200

    Profile details Binding

--- PhisilyncFinal/Services/LocalDb.cs
+++ PhisilyncFinal/Services/LocalDb.cs
@@ -681,5 +681,12 @@ namespace PhisilyncFinal.Services
         {
             return _dbConnection.Table<User>().FirstOrDefault();
         }
+
+        public User UpdateUser(User user)
+        {             
+            _dbConnection.Update(user);
+            return user;
+        }
+
     }
 } 
