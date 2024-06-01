4546e5b378b2fafcba36df68692191674e8b6a5b
TayiebK <150594024+mtk2608@users.noreply.github.com>
2024-06-01 14:50:46 +0200

    UI Changes and Binding

--- PhisilyncFinal/ViewModels/EditProfileVM.cs
+++ PhisilyncFinal/ViewModels/EditProfileVM.cs
@@ -1,14 +1,50 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using PhisilyncFinal.Services;
+using PhisilyncFinal.Models;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.ApplicationModel;
 
 namespace PhisilyncFinal.ViewModels
 {
-    public partial class EditProfileVM
+    public partial class EditProfileVM : BaseViewModel
     {
+        private LocalDb _localDb;
+
+        private User _currentUser;
+
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set
+            {
+                _currentUser = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private ImageSource _avatar;
+
+        public ImageSource Avatar
+        {
+            get { return _avatar; }
+            set
+            {
+                _avatar = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public EditProfileVM()
+        {
+            _localDb = new();
+            CurrentUser = _localDb.GetUserByID(1);
+        }
+
         [RelayCommand]
         private async Task ProfileDetails()
         {
@@ -26,5 +62,23 @@ namespace PhisilyncFinal.ViewModels
         {
             await Shell.Current.GoToAsync("Settings");
         }
+
+        [RelayCommand]
+        private async Task ChangeAvatar()
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+                Avatar = ImageSource.FromStream(() => stream);
+
+            }
+        }
+
+
     }
 }
